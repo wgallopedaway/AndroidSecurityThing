f004b1da0683877816b0c47ef27a2ba2f72af7b5
Byron Alleman <byron.alleman@gmail.com>
2012-02-08 17:31:27 -0500

    Created and Android project using eclipse.  Min SDK version is 7, meaning the user must have Android 2.1 on their phone to use the application.  Seemed like a good idea.

---
+++ src/com/csce548/securitything/AndroidSecurityThingActivity.java
@@ -0,0 +1,13 @@
+package com.csce548.securitything;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidSecurityThingActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
